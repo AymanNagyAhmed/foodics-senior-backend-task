32355559f3daf4881c4ff72f0d2a21a5dfca9eae
Jaques Candeias <jaques@cronic.pt>
2024-07-10 14:00:24 +0100

    add retention_rate to branch model factory

--- database/factories/BranchFactory.php
+++ database/factories/BranchFactory.php
@@ -26,6 +26,7 @@ class BranchFactory extends Factory
     {
         return [
             'name' => $this->faker->word,
+            'retention_rate' => fake()->numberBetween(10, 90),
         ];
     }
 }
