521399c3a5129aa73634adba5ba988b0cd21444f
AymanNagyAhmed <aymannagy.ahmed@gmail.com>
2024-08-20 18:08:06 +0300

    Add validation and error handling to StoreOrderRequest

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Helpers\ResponseHelper;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class StoreOrderRequest extends FormRequest
 {
@@ -22,7 +25,25 @@ class StoreOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string'],
+            'branch_id' => ['required', 'integer', 'exists:branches,id'],
+            'items' => ['required', 'array', 'min:1'],
+            'items.*.product_id' => ['required', 'exists:products,id','distinct'],
+            'items.*.quantity' => ['required', 'integer', 'min:1'],
+            'items.*.price' => ['required', 'numeric', 'min:0'],
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): HttpResponseException
+    {
+        throw new HttpResponseException(
+            ResponseHelper::failResponse('Validation failed', $validator->errors(), 422)
+        );
     }
 }
