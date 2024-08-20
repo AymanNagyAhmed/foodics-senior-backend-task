448304933160dfa6fde6162ce87995c9c7c35618
AymanNagyAhmed <aymannagy.ahmed@gmail.com>
2024-08-20 18:22:17 +0300

    refactor: API routes for creating orders with rate limiting middleware

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Middleware\RateLimitCreateOrder;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('/products', App\Http\Controllers\ProductController::class);
-Route::apiResource('/orders', App\Http\Controllers\OrderController::class);
+
+Route::post('/orders', [OrderController::class, 'store'])->middleware(RateLimitCreateOrder::class);
+Route::apiResource('/orders', OrderController::class)->except(['store']);
+
 Route::apiResource('/order_items', App\Http\Controllers\OrderItemController::class);
